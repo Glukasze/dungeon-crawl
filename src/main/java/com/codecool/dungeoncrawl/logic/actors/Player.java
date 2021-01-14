a38733e40ec82a2493e9dc3d6d8805c5a48feca9
Glukasze <Mortal^66>
2021-01-14 22:53:12 +0100

    Implementing inventory
    -inventory is visible on the screen
    -every picked item is shown in the inventory

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -1,10 +1,36 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.items.Item;
+
+import java.util.ArrayList;
 
 public class Player extends Actor {
+
+    private ArrayList<Item> inventory = new ArrayList<Item>();
+
     public Player(Cell cell) {
         super(cell);
+    }
+
+    public void addToInventory(Item item) {
+        this.inventory.add(item);
+    }
+
+    public void removeFromInventory(Item item) {
+        this.inventory.remove(item);
+    }
+
+    public ArrayList<Item> getInventory() {
+        return this.inventory;
+    }
+
+    public String getInventoryAsString() {
+        String result = "";
+        for (Item item : inventory) {
+            result += (item.getTileName() + "\n");
+        }
+        return result;
     }
 
     public String getTileName() {
